4b5a43b8c8853f789aaa03c9f1a2f1dd52604a1a
GustavoAzHenrique <ghazevedoctt@gmail.com>
2024-06-21 00:43:13 -0300

    aplicacao-melhoria-continua-estg

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,6 +9,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { LoginService } from '../services/login-service/login.service';
 import { Router } from '@angular/router';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-login',
@@ -20,13 +21,15 @@ import { Router } from '@angular/router';
     MatIconModule,
     MatFormFieldModule,
     ReactiveFormsModule,
-    CommonModule
+    CommonModule,
+    
   ],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
   loginForm: FormGroup;
+  
 
   constructor(private fb: FormBuilder, private loginService: LoginService, private router: Router) {
     this.loginForm = this.fb.group({
@@ -37,12 +40,14 @@ export class LoginComponent {
 
   onSubmit(): void {
     if (this.loginForm.valid) {
+      
       const { email, password } = this.loginForm.value;
       this.loginService.login(email, password).subscribe({
         next: (e) => {
           this.router.navigate(['/pagina-inicial']);
         },
         error: (err) => {
+          
           console.error(err);
           alert('Este e-mail não tem as permissões necessárias para acesso.');
         },
--- src/app/pagina-inicial/pagina-inicial.component.ts
+++ src/app/pagina-inicial/pagina-inicial.component.ts
@@ -6,13 +6,15 @@ import { MAT_DATE_LOCALE } from '@angular/material/core';
 import { LoginService } from '../services/login-service/login.service';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCard } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   standalone: true,
   selector: 'app-main',
   templateUrl: './pagina-inicial.component.html',
   styleUrls: ['./pagina-inicial.component.scss'],
-  imports: [CommonModule, FuncionarioFormComponent, FuncionarioListComponent, MatButtonModule, MatCard],
+  imports: [CommonModule, FuncionarioFormComponent, FuncionarioListComponent, MatButtonModule, MatCard, MatIconModule, MatMenuModule],
   providers: [{ provide: MAT_DATE_LOCALE, useValue: 'pt-BR' }]
 })
 export class PaginaInicialComponent {
